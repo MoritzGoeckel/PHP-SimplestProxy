f0555254ce8c1c597f7582d7a7cc258face2c58d
Moritz Göckel <moritz.goeckel@gmail.com>
2015-06-25 00:44:15 +0200

    Added curl

--- proxy.php
+++ proxy.php
@@ -1,8 +1,24 @@
 <?php
 
+function url_get_contents ($Url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $Url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
+
 if(isset($_GET['url'])){
     $url = urldecode($_GET['url']);
     echo file_get_contents($url);
+	
+	if(ini_get('allow_url_fopen'))
+        echo file_get_contents($url);
+    elseif(function_exists('curl_init'))
+    	echo url_get_contents ($url);
+    else
+		echo "<h2>Error:</h2>allow_url_fopen and curl are not enabled on the server.<br />Enable at least one of these for the proxy to work.";
 }
 else {
     echo "<h2>Error:</h2>Add an GET parameter named 'url' with the encoded url as value";
