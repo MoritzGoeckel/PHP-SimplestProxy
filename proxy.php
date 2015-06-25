2365dc9b3d2ebc4fc4bb89c8065f7083be0a9921
Moritz Göckel <moritz.goeckel@gmail.com>
2015-06-25 09:34:48 +0200

    Renamed a function

--- proxy.php
+++ proxy.php
@@ -1,6 +1,6 @@
 <?php
 
-function url_get_contents ($Url) {
+function curl_get_contents ($Url) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $Url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -13,10 +13,10 @@ if(isset($_GET['url'])){
     $url = urldecode($_GET['url']);
     echo file_get_contents($url);
 	
-	if(ini_get('allow_url_fopen'))
+    if(ini_get('allow_url_fopen'))
         echo file_get_contents($url);
     elseif(function_exists('curl_init'))
-    	echo url_get_contents ($url);
+    	echo curl_get_contents ($url);
     else
 		echo "<h2>Error:</h2>allow_url_fopen and curl are not enabled on the server.<br />Enable at least one of these for the proxy to work.";
 }
